d0255d4ebd806f6bc1fadfd6c49bd1ef1734eb2c
Iakwvos <53518900+Iakwvos@users.noreply.github.com>
2024-09-27 14:23:02 +0300

    First

---
+++ helloworld/urls.py
@@ -0,0 +1,8 @@
+# helloworld/urls.py
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('hello.urls')),  # Include hello app's URLs
+]
