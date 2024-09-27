09a4b57f292d5c6fe858a421fbc68ba9aa690f20
Iakwvos <iakwvos99@gmail.com>
2024-09-27 17:44:50 +0300

    first

--- pagepilot/urls.py
+++ pagepilot/urls.py
@@ -3,9 +3,7 @@ from django.urls import path, include
 from main_app import views as main_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', main_views.main_page, name='main_page'),
     # Add any other routes as needed
 ]
-
-handler404 = 'main_app.views.handler404'
-handler500 = 'main_app.views.handler500'
