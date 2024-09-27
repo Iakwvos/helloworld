02e6c6175d4e33becca0c51894dd891ef392a6cb
Iakwvos <53518900+Iakwvos@users.noreply.github.com>
2024-09-27 15:04:21 +0300

    Delete helloworld directory

--- helloworld/urls.py
+++
@@ -1,8 +0,0 @@
-# helloworld/urls.py
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hello.urls')),  # Include hello app's URLs
-]
