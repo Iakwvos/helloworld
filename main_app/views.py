3ac98855af4527bf5ba75e768d8ff6219eefdec1
Iakwvos <iakwvos99@gmail.com>
2024-09-27 17:35:06 +0300

    c

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 # hello/views.py
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def pagepilot(request):
-    return HttpResponse("Hello, World!")
+def main_page(request):
+    """Render the main landing page."""
+    return render(request, 'main_page.html')
