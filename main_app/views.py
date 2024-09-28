a681c2b781bb68b1d8a0c64f971ac7d1c6b28e7f
Iakwvos <iakwvos99@gmail.com>
2024-09-28 14:57:03 +0300

    Test

--- main_app/views.py
+++ main_app/views.py
@@ -36,19 +36,15 @@ logger.setLevel(logging.DEBUG)  # Set to DEBUG for detailed logs
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler(os.path.join(settings.BASE_DIR, 'app.log'))
-file_handler.setLevel(logging.ERROR)  # Log errors to file
 
 # Create formatters and add to handlers
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
 
 # Add handlers to the logger
 logger.addHandler(console_handler)
-logger.addHandler(file_handler)
 
 PLAN_LIMITS = {
     'Free': {
