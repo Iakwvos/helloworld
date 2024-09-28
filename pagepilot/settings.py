e3432052f533da7b7dc30a9bf03cfd4d85178dad
Iakwvos <iakwvos99@gmail.com>
2024-09-28 15:18:26 +0300

    Working DB

--- pagepilot/settings.py
+++ pagepilot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 # Initialize environment variables
 env = environ.Env()
@@ -83,10 +84,9 @@ WSGI_APPLICATION = 'pagepilot.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
